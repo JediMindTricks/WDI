68b4d6b3d0b124439cb91ef105c45b6e1a499389
Michael <Michael.Bardwell.Scott@Gmail.com>
2016-11-15 23:10:57 -0500

    Day 2
    HTML & CSS fundementals

---
+++ WDI-Workspace/Week-01/Day-02/MickeyMouseWithHat/sketch.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(480, 360);  // width and height
+    rect(50, 50, 100, 50); // top of hat
+    rect(0, 100, 200, 50); //
+    ellipse(100, 250, 200, 200); // face
+    ellipse(200, 150, 100, 100); // face
+    ellipse(100, 150, 100, 100); // face
+}
+
+function draw() {
+
+}
