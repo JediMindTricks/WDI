1ca21a15ce1505b807a83df619dbfbaa2871c789
Michael <Michael.Bardwell.Scott@Gmail.com>
2016-11-19 11:40:09 -0500

    Day 5 update

--- WDI-Workspace/Week-01/Day-05/functions/js/app.js
+++ WDI-Workspace/Week-01/Day-05/functions/js/app.js
@@ -2,13 +2,28 @@
 var name = prompt("What is your  name?");
 
 function greet(name) {
-	console.log("Hello, " + name);
-}
+	console.log("Hello, " + name)
+};
 
 greet(name);
 
 function sayGoodbye(name) {
-	console.log("Bye, have a wonderful time!");
-}
+	console.log("Bye, have a wonderful time " + name + "!")
+};
 
 sayGoodbye(name);
+
+function lastLetter(word) {
+	console.log(word[word.length -1]);
+}
+
+function halfWord(word) {
+	var length = word.length - 1;
+	var half = length / 2;
+
+	console.log(word.substring(0, half));
+}
+
+lastLetter("Wassup");
+halfWord("Assembly");
+
